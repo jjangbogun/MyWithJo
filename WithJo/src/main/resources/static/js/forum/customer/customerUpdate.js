408064808e5e85b2bacbfe6283d83634569bbc37
jjangbogun <khddhk123@naver.com>
2024-10-25 18:02:28 +0900

    잘못올려서 지움

--- WithJo/src/main/resources/static/js/forum/customer/customerUpdate.js
+++
@@ -1,7 +0,0 @@
-function customerCheckFnc() {
- 	let customerAns2 = $('#customerAns2').val();
-	customerAns2 = customerAns2.replaceAll(/'/g, '`');
-	customerAns2 = customerAns2.replaceAll(/"/g, '&quot;');
-	$('#customerAns').val(customerAns2);	
-	document.getElementById("customerForm").submit();  
-}
