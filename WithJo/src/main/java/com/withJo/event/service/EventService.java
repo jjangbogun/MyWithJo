bbc5fc84d3e200394b1a88a304919fcdb38f76e7
jjangbogun <khddhk123@naver.com>
2024-11-06 17:55:15 +0900

    깃허브 오류수정 -보근

--- WithJo/src/main/java/com/withJo/event/service/EventService.java
+++ WithJo/src/main/java/com/withJo/event/service/EventService.java
@@ -3,11 +3,12 @@ package com.withJo.event.service;
 import java.util.List;
 
 import com.withJo.event.domain.EventVo;
-import com.withJo.notice.domain.NoticeVo;
 
 
 public interface EventService {
 	
-	List<EventVo> eventSelectList();	
-	public int eventUpdateOne(EventVo eventVo);
+	List<EventVo> eventSelectList();
+
+	int eventUpdateOne(EventVo eventVo);	
+	
 }
